1c8dd2ba3e10065c173b8f65f3ef990d7a13d94f
Fernando León <142443191+FernandoLeonDelRio@users.noreply.github.com>
2026-04-15 15:00:39 -0600

    Update conex.php

--- conex.php
+++ conex.php
@@ -6,7 +6,7 @@ if (session_status() == PHP_SESSION_NONE) {
 // Único usuario MySQL para toda la aplicación
 define('DB_USER', 'proydweb_bd2026');
 define('DB_PASS', 'DWeb_p2@26');
-define('DB_NAME', '2doAvance');
+define('DB_NAME', 'proydweb_p2026');
 
 function Conectarse() {
     $link = @mysqli_connect("localhost", DB_USER, DB_PASS, DB_NAME);
