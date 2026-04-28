f7517a4d1102d3c823f1d10315216a0aa56d8232
John <arellanojohnny250@gmail.com>
2026-04-27 20:11:37 -0600

    Agregar Butones
    Agregar Paginacion

--- barra_lateral/Catalogo.php
+++ barra_lateral/Catalogo.php
@@ -5,8 +5,21 @@ include("../conex.php");
 // 1. Conexión a la base de datos
 $conexion = Conectarse();
 
+
+$productos_por_pagina = 14;
+$pagina_actual = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+if ($pagina_actual < 1) $pagina_actual = 1;
+$offset = ($pagina_actual - 1) * $productos_por_pagina;
+
+$query_total = "SELECT COUNT(*) AS total FROM t_productos";
+$res_total = mysqli_query($conexion, $query_total);
+$fila_total = mysqli_fetch_assoc($res_total);
+$total_productos = $fila_total['total'];
+$total_paginas = ceil($total_productos / $productos_por_pagina);
+
+
 // 2. Consulta para traer todos los productos
-$query = "SELECT id, nombre, precio, imagen FROM t_productos";
+$query = "SELECT id, nombre, precio, imagen FROM t_productos LIMIT $productos_por_pagina OFFSET $offset";
 $resultado = mysqli_query($conexion, $query);
 ?>
 <!DOCTYPE html>
@@ -68,10 +81,9 @@ $resultado = mysqli_query($conexion, $query);
                     while ($producto = mysqli_fetch_assoc($resultado)) { 
                         
                         // EL TRUCO: Extraemos solo el nombre de la foto (ej: "TacoArabe.png")
-                        $nombre_foto = basename($producto['imagen']);
-                        
+                        $nombre_foto = basename($producto['imagen'] ?? '');
                         // Construimos la ruta EXACTA que usabas en tu HTML manual
-                        $ruta_segura = "../Imagenes/Productos/" . $nombre_foto;
+                            $ruta_segura = "../Imagenes/Productos/" . $nombre_foto;                        
                 ?>
                         <div class="producto-card">
                             <div class="producto-img-container">
@@ -83,21 +95,38 @@ $resultado = mysqli_query($conexion, $query);
                                 <div class="producto-precio">$<?php echo floatval($producto['precio']); ?></div>
                             </div>
                         </div>
+
                 <?php 
                     } 
                 } else {
                     echo "<p>No hay productos registrados en el catálogo aún.</p>";
                 }
-                
                 // Cerramos la conexión
                 if(isset($conexion)) { mysqli_close($conexion); }
                 ?>
 
+            </div> <!-- cierra productos-grid -->
+
+            <?php if ($total_paginas > 1): ?>
+            <div class="paginacion">
+                <?php if ($pagina_actual > 1): ?>
+                    <a href="Catalogo.php?pagina=<?php echo $pagina_actual - 1; ?>" class="flecha">&#8592;</a>
+                <?php else: ?>
+                    <span class="flecha disabled">&#8592;</span>
+                <?php endif; ?>
+
+                <span class="pagina-info">Página <?php echo $pagina_actual; ?> de <?php echo $total_paginas; ?></span>
+
+                <?php if ($pagina_actual < $total_paginas): ?>
+                    <a href="Catalogo.php?pagina=<?php echo $pagina_actual + 1; ?>" class="flecha">&#8594;</a>
+                <?php else: ?>
+                    <span class="flecha disabled">&#8594;</span>
+                <?php endif; ?>
             </div>
-        </div>
-    </div>
+            <?php endif; ?>
 
-
+        </div> 
+    </div> 
 
 </body>
 
